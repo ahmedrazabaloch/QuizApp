a632b030e0860b99ebf312b99c2d9610de50ff63
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-19 02:07:56 +0500

    questions added

---
+++ app.js
@@ -0,0 +1,52 @@
+var questions = [
+  {
+    question: "What HTML tag is used to create an ordered (numbered) list?",
+    option1: "<ul>",
+    option2: "<li>",
+    option3: "<ol>",
+    option4: "<dl>",
+    answer: "<ol>",
+  },
+  {
+    question:
+      "In CSS, what property is used to change the text color of an element?",
+    option1: "background-color",
+    option2: "font-style",
+    option3: "color",
+    option4: "text-align",
+    answer: "color",
+  },
+  {
+    question: "How do you declare a variable in JavaScript?",
+    option1: "variable x;",
+    option2: "let x;",
+    option3: "var x;",
+    option4: "Both b and c",
+    answer: "Both b and c",
+  },
+  {
+    question:
+      "Which HTML element is used to embed external content, such as a video or audio file, on a web page?",
+    option1: "<iframe>",
+    option2: "<embed>",
+    option3: "<object>",
+    option4: "<video>",
+    answer: "<video>",
+  },
+  {
+    question: "What is the box model in CSS?",
+    option1: "A model for designing 3D shapes in CSS.",
+    option2: "A way to organize content using boxes in HTML.",
+    option3:
+      "A model that describes how elements are rendered as boxes with content, padding, borders, and margins.",
+    option4: "A model for creating rounded corners in CSS.",
+    answer:
+      "A model that describes how elements are rendered as boxes with content, padding, borders, and margins.",
+  },
+];
+
+var index = 0;
+
+function renderquestions(){
+    
+}
