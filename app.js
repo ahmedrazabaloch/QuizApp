e3f7e188d2109bdd6e4afb3d2a8c203a406ba9ff
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-19 03:13:41 +0500

    quiz added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var questions = [
   {
     question: "What HTML tag is used to create an ordered (numbered) list?",
-    option1: "<ul>",
-    option2: "<li>",
-    option3: "<ol>",
-    option4: "<dl>",
-    answer: "<ol>",
+    option1: "&lt;ul&gt;",
+    option2: "&lt;li&gt;",
+    option3: "&lt;ol&gt;",
+    option4: "&lt;dl&gt;",
+    answer: "&lt;ol&gt;",
   },
   {
     question:
@@ -27,11 +27,11 @@ var questions = [
   {
     question:
       "Which HTML element is used to embed external content, such as a video or audio file, on a web page?",
-    option1: "<iframe>",
-    option2: "<embed>",
-    option3: "<object>",
-    option4: "<video>",
-    answer: "<video>",
+    option1: "&lt;iframe&gt;",
+    option2: "&lt;embed&gt;",
+    option3: "&lt;object&gt;",
+    option4: "&lt;video&gt;",
+    answer: "&lt;video&gt;",
   },
   {
     question: "What is the box model in CSS?",
@@ -46,7 +46,37 @@ var questions = [
 ];
 
 var index = 0;
+var score = 0;
 
-function renderquestions(){
-    
+function renderQuestions() {
+  var question = document.getElementById("qustionsContainer");
+  var options = document.getElementsByName("options");
+
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].checked) {
+      if (options[i].value === questions[index - 1].answer) {
+        score++;
+      }
+    }
+  }
+  if (!questions[index]) {
+    console.log(score);
+    return;
+  }
+  var questionValue = questions[index];
+  question.innerHTML = `
+        <div id="qustions">
+            <span>1.</span>
+            <p>${questionValue.question}</p>
+        </div>
+        <div class="options_list">
+            <label for="options1" class="options"><input type="checkbox" id="options1" name="options" value="${questionValue.option1}">${questionValue.option1}<span class="checkmark"></span></label>
+            <label for="options2" class="options"><input type="checkbox" id="options2" name="options" value="${questionValue.option2}">${questionValue.option2}<span class="checkmark"></span></label>
+            <label for="options3" class="options"><input type="checkbox" id="options3" name="options" value="${questionValue.option3}">${questionValue.option3}<span class="checkmark"></span></label>
+            <label for="options4" class="options"><input type="checkbox" id="options4" name="options" value="${questionValue.option4}">${questionValue.option4}<span class="checkmark"></span></label>
+        </div>
+  `;
+  index++;
 }
+
+renderQuestions();
