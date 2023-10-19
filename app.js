53ce55c4a21d171a5abd52e71296a8c1097b74c6
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-20 03:57:49 +0500

    Quzi App ready

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var questions = [
   {
     question: "What HTML tag is used to create an ordered (numbered) list?",
-    option1: "&lt;ul&gt;",
-    option2: "&lt;li&gt;",
-    option3: "&lt;ol&gt;",
-    option4: "&lt;dl&gt;",
-    answer: "&lt;ol&gt;",
+    option1: "ul",
+    option2: "li",
+    option3: "ol",
+    option4: "dl",
+    answer: "ol",
   },
   {
     question:
@@ -27,11 +27,11 @@ var questions = [
   {
     question:
       "Which HTML element is used to embed external content, such as a video or audio file, on a web page?",
-    option1: "&lt;iframe&gt;",
-    option2: "&lt;embed&gt;",
-    option3: "&lt;object&gt;",
-    option4: "&lt;video&gt;",
-    answer: "&lt;video&gt;",
+    option1: "iframe",
+    option2: "embed",
+    option3: "object",
+    option4: "video",
+    answer: "video",
   },
   {
     question: "What is the box model in CSS?",
@@ -52,9 +52,17 @@ var hideBtn = login_form.querySelector(".hideBtn");
 var passIcon = login_form.querySelector(".showPass");
 var userEmail = document.getElementById("uEmail");
 var userPass = document.getElementById("uPass");
-
+var infoBox = document.querySelector(".info_box");
+var quizStart = document.querySelector(".quiz_container");
+var resultbox = document.querySelector(".result_box");
+var scoreText = document.querySelector(".score_text");
 var email = "ahmed@gmail.com";
 var pass = "12345";
+var countDown = document.getElementById("timer");
+var index = 0;
+var score = 0;
+var counter;
+var timeValue = 5;
 
 function showPopup() {
   emailPass.style.display = "flex";
@@ -66,32 +74,54 @@ function backToForm() {
 }
 function loginForm() {
   if (userEmail.value === email && userPass.value === pass) {
-    startBtn.style.display = "block";
+    infoBox.style.display = "block";
     login_form.style.display = "none";
   } else {
     passIcon.style.color = "#007bff";
     alert("wrong password");
   }
 }
+function quit() {
+  location.reload();
+}
+function enterQuiz() {
+  infoBox.style.display = "none";
+  startBtn.style.display = "block";
+}
+function startQuiz() {
+  quizStart.style.display = "block";
+  startBtn.style.display = "none";
+  renderQuestions();
+}
 
-var countDown = document.getElementById("timer");
-var index = 0;
-var score = 0;
-var counter;
+function restart() {
+  location.reload();
+  login_form.style.display = "none";
+  infoBox.style.display = "none";
+  startBtn.style.display = "block";
+}
 
 function renderQuestions() {
   var question = document.getElementById("qustionsContainer");
   var options = document.getElementsByName("options");
-
+  var qustionNo = document.getElementById("qustionNo");
+  clearInterval(counter);
+  startTimer(timeValue);
   for (var i = 0; i < options.length; i++) {
     if (options[i].checked) {
       if (options[i].value === questions[index - 1].answer) {
         score++;
+        console.log(score);
       }
     }
   }
   if (!questions[index]) {
-    console.log(score);
+    clearInterval(counter);
+    resultbox.style.display = "flex";
+    quizStart.style.display = "none";
+    scoreText.innerHTML = `
+   <span>Score: <p>${score}</p>out of<p>${options.length + 1}</p><span>
+    `;
     return;
   }
   var questionValue = questions[index];
@@ -108,14 +138,17 @@ function renderQuestions() {
         </div>
   `;
   index++;
+  qustionNo.innerHTML = index;
 }
 
-renderQuestions();
-startTimer(10);
 function startTimer(time) {
   counter = setInterval(timer, 1000);
   function timer() {
     countDown.textContent = time;
     time--;
+    if (time < 0) {
+      clearInterval(counter);
+      renderQuestions();
+    }
   }
 }
